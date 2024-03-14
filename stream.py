de87872631f274e865c57f6479c3885545cdec47
Deni Luqmantoro <101716699+DeniLuqmantoro@users.noreply.github.com>
2024-03-14 14:37:01 +0700

    stream.py

--- stream.py
+++ stream.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
